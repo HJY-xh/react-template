e610f2c40d3f9aafbb56465647d4ae8e4b696d4b
黄建宇 <huangjianyu@lightinthebox.com>
2021-04-08 20:18:53 +0800

    eslint & prettier

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -3,24 +3,24 @@ const baseConfig = require("./webpack.config.base");
 
 // 通过webpack-merge合并基础配置，添加生产时配置
 const webpackConfig = merge(baseConfig, {
-    mode: "production", // 生产模式
-    stats: {
-        children: false, // webpack打包时子模块信息设置不显示
-        warnings: false, // 警告不显示
-    },
-    optimization: {
-        // splitChunks 用来避免模块之间重复的依赖关系
-        splitChunks: {
-            cacheGroups: {
-                commons: {
-                    name: "commons",
-                    chunks: "initial",
-                    minChunks: 3,
-                    enforce: true,
-                },
-            },
-        },
-    },
+	mode: "production", // 生产模式
+	stats: {
+		children: false, // webpack打包时子模块信息设置不显示
+		warnings: false // 警告不显示
+	},
+	optimization: {
+		// splitChunks 用来避免模块之间重复的依赖关系
+		splitChunks: {
+			cacheGroups: {
+				commons: {
+					name: "commons",
+					chunks: "initial",
+					minChunks: 3,
+					enforce: true
+				}
+			}
+		}
+	}
 });
 
 module.exports = webpackConfig;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Koa from "koa";
 const app = new Koa();
 
 app.use(async (ctx) => {
-    ctx.body = "hello koa";
+	ctx.body = "hello koa";
 });
 
 // 监听端口
